89820ba29d641f2ce64ce722072efd3d97a24c75
passLsk <471627950@qq.com>
2022-07-13 11:49:57 +0800

    初始化了人资项目

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人资管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 固定定位header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否显示
+   */
+  sidebarLogo: true
+}
